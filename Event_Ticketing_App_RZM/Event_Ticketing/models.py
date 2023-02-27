2bd272c5133c77d321b11bc536e66b4ac67be852
mahood101 <77034889+mahood101@users.noreply.github.com>
2023-02-27 13:11:29 -0500

    database created and updated
    database created and updated

--- Event_Ticketing_App_RZM/Event_Ticketing/models.py
+++ Event_Ticketing_App_RZM/Event_Ticketing/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Meets(models.Model):
+    # Meet ID is generated automatically
+    meet_name = models.CharField(max_length=75, unique=True)
+    meet_date = models.DateField()
+    max_capacity = models.IntegerField()
+    creation_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.meet_name
+
+class Spectators(models.Model):
+    # Spectator ID is generated automatically
+    spectator_fname = models.CharField(max_length=75)
+    spectator_lname = models.CharField(max_length=75)
+    purchase_time = models.DateTimeField(auto_now_add=True)
+    ticket_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    seat_num = models.CharField(max_length=10)
+    # seatType look into how to set it up
+    meet_id = models.ForeignKey(Meets, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.spectator_fname + " " + self.spectator_lname
