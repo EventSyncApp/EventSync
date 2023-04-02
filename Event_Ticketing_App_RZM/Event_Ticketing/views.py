1deec18b66a88fb45ebd578680cfd54193194984
zbroderick3 <111665969+zbroderick3@users.noreply.github.com>
2023-04-01 22:37:02 -0400

    Email in progress

--- Event_Ticketing_App_RZM/Event_Ticketing/views.py
+++ Event_Ticketing_App_RZM/Event_Ticketing/views.py
@@ -4,6 +4,31 @@ from rest_framework import viewsets
 from .models import Meets
 from .serializers import HomeSerializer
 
+
+import mysql.connector
+
+def send_email(request, id):
+    # Connect to the MySQL database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user='root',
+        password='rzmApplications124',
+        database='ticketingdb'
+    )
+
+    mycursor = mydb.cursor()
+
+    # Retrieve the email address from the database using the given id
+    mycursor.execute("SELECT email FROM customers WHERE id = %s", (id,))
+    result = mycursor.fetchone()
+    email = result[0]
+    mydb.close()
+    
+    # Call the send_mail function with the retrieved email address
+    send_mail(email)
+
+
+
 # Create your views here.
 
 # REST API class for handling http requests
