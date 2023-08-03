b7bd79920c3845625a96fbaf0a1e7f04d60ed2e0
zbroderick3 <111665969+zbroderick3@users.noreply.github.com>
2023-08-02 22:32:35 -0400

    added new model fields

--- Event_Ticketing_App_RZM/Event_Ticketing/models.py
+++ Event_Ticketing_App_RZM/Event_Ticketing/models.py
@@ -1,16 +1,35 @@
 from django.db import models
 from datetime import datetime
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 #function for getting purchase time
 def get_current_time():
     return timezone.now()
+
+#function for converting meet time
+def validate_time_format(value):
+    try:
+        # Attempt to parse the input value as time with format "HH:MM"
+        hour, minute = value.split(":")
+        hour, minute = int(hour), int(minute)
+        if not (0 <= hour <= 23) or not (0 <= minute <= 59):
+            raise ValueError
+    except ValueError:
+        raise ValidationError('Invalid time format. Time should be in "HH:MM" format.')
 
 # Create your models here.
 class Meets(models.Model):
     # Meet ID is generated automatically
     meet_name = models.CharField(max_length=75, unique=True)
     meet_date = models.DateField()
+    meet_time_start = models.CharField(max_length=5, validators=[validate_time_format], default='00:00')
+    meet_location_venue = models.CharField(max_length=75, unique=False, default='Chiseled Life')
+    meet_location_address = models.CharField(max_length=75, unique=False, default='123 Broderick Ln')
+    meet_location_city = models.CharField(max_length=75, unique=False, default='Columbia')
+    meet_location_state = models.CharField(max_length=2, default="MD")
+    meet_location_zipcode = models.CharField(max_length=5, unique=False, default='00000')
+    meet_about_text = models.TextField(max_length=1200, default='Please add a paragraph about the meet.')
     max_capacity = models.IntegerField()
     creation_time = models.DateTimeField(auto_now_add=True)
 
