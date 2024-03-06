749195decc034188e047e57c65cb02ca8b7c63c0
Isaiah Ofalt <ilofalt@gmail.com>
2024-03-05 22:12:44 -0700

    Added MUI to home_page

--- event_ticketing_react/src/components/Home_Page.js
+++ event_ticketing_react/src/components/Home_Page.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { format } from 'date-fns';
+import { Typography, List, ListItem, ListItemText } from '@mui/material';
+
 
 const HomeList = () => {
     const [data, setData] = useState([]);
@@ -20,13 +22,20 @@ const HomeList = () => {
 
     return (
         <div>
-            <h1>Meet List</h1>
-            <ul>
-                {data.map(item => (
-                    <li><b>{item.meet_name}</b> - {format(new Date(item.meet_date), 'MMMM d, yyyy')}</li>
-                ))}
-            </ul>
-        </div>
+             <Typography variant="h4" gutterBottom component="h1">
+               Meet List
+             </Typography>
+             <List>
+               {data.map((item, index) => (
+                 <ListItem key={index} divider>
+                   <ListItemText
+                     primary={item.meet_name}
+                     secondary={format(new Date(item.meet_date), 'MMMM d, yyyy')}
+                   />
+                 </ListItem>
+               ))}
+             </List>
+           </div>
     );
 };
 
